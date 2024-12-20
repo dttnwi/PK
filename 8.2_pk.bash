927628540493d759d67d09a7f78001bfa6f5b82c
dttnwi <katerina.06.illarionova@yandex.ru>
2024-12-20 22:07:07 +0300

    Create 8.2_pk.bash

---
+++ 8.2_pk.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Пожалуйста, укажите директорию и расширение файлов :)"
+    exit 1
+fi
+DIRECTORY=\$1          
+RACSHIRENIE=\$2      
+if [ ! -d "$DIRECTORY" ]; then
+    echo "Директория не найдена! :("
+    exit 1
+fi
+
+# Поиск файлов с указанным расширением
+FILES=$(find "$DIRECTORY" -type f -name "*.$RACSHIRENIE")
+if [ -z "$FILES" ]; then
+    echo "Файлы с расширением $RACSHIRENIE не найдены. :("
+else
+    echo "Найденные файлы с расширением $RACSHIRENIE:"
+    echo "$FILES"
+fi
